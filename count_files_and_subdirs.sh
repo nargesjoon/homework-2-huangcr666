7f687a66ec59b1d0eaf38c4475be82154ebce226
Chaoran Huang <huangcr@users.campus>
2019-09-18 15:15:15 -0500

    new hw#2

--- count_files_and_subdirs.sh
+++ count_files_and_subdirs.sh
@@ -1,7 +1,10 @@
-echo "Files:"
-find . -type f | wc -l
-echo "Directories:"
-find . -type d | wc -l
+echo "number of files:"
+find . -maxdepth 1 -type f | wc -l
+echo "number of directories:"
+find . -maxdepth 1 -type d | wc -l
+
+
+
 
 ####test####
 
