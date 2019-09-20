9b1a44317acbe04103dcf44a5712bd299f08a91d
huangcr <huangcr@ksu.edu>
2019-09-20 18:02:24 -0500

    hw2

--- temperature.sh
+++ temperature.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Please enter temperatrue in Fahrenheit:"
 read f
-c=$(echo "scale=3;(5/9)*($f-32)"|bc)
+c=$(echo "scale=2;(5/9)*($f-32)"|bc)
 ###c=$(echo "(5/9)*($f-32)"|bc)
 echo "$f F = $c C"
